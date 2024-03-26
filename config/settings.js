48fc075c8995d24712d5850aee631a8ad9ced1b4
Kyle Holly <kholly@stevens.edu>
2024-03-25 20:22:44 -0400

    Initial Commit

---
+++ config/settings.js
@@ -0,0 +1,6 @@
+// You will need to change the DB name to match the required DB name in the assignment specs!
+export const mongoConfig = {
+    serverUrl: 'mongodb://localhost:27017/',
+    database: 'CS546_Group2_Final_Project'
+  };
+  
---
+++ data/reviews.js
@@ -0,0 +1,17 @@
+
+const createReview = async (
+    rating,
+    reviewText
+) => {};
+
+const getAllReviews = async () => {};
+
+const getReview = async (reviewId) => {};
+
+const removeReview = async (reviewId) => {};
+
+const updateReview = async (
+    reviewId,
+    rating,
+    reviewText
+) => {};
